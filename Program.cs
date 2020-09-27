eac5f40ae4ed6c5715391dd75db01a06ba4a03c0
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-27 17:20:16 -0300

    resolucao do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcule o montante final de um investimento a juros simples.!");
+            double c = 1200.00;
+            double i = 0.02;
+            double t = 15;
+            double juros = c * i * t;
+            double Montante = c + juros;
+            Console.WriteLine($"{c} * {i} * {t} = {juros:C}");
+            Console.WriteLine($"{c} + {juros} = {Montante:C}");
+
+
+        }
+    }
+}
